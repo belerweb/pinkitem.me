5419566617ff237166ad1bf02a2bbf9b61db0d1e
belerweb <belerweb@gmail.com>
2013-08-14 21:32:04 +0800

    add wp config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', $_ENV['PINKITEM_DB_NAME']);
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', $_ENV['PINKITEM_DB_USER']);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', $_ENV['PINKITEM_DB_PASSWORD']);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $_ENV['PINKITEM_DB_HOST']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         $_ENV['PINKITEM_AUTH_KEY']);
+define('SECURE_AUTH_KEY',  $_ENV['PINKITEM_SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY',    $_ENV['PINKITEM_LOGGED_IN_KEY']);
+define('NONCE_KEY',        $_ENV['PINKITEM_NONCE_KEY']);
+define('AUTH_SALT',        $_ENV['PINKITEM_AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['PINKITEM_SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT',   $_ENV['PINKITEM_LOGGED_IN_SALT']);
+define('NONCE_SALT',       $_ENV['PINKITEM_NONCE_SALT']);
 
 /**#@-*/
 
